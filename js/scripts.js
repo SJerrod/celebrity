39c5b0fd27b6a2abe599ed11db6da96dda1f4046
Joseph Pearce <joseph.pearce05@gmail.com>
2020-06-04 15:05:44 -0700

    add questins, JS logic
    Co-authored-by:  Jerrod Styrk <jstyrk@citadel.edu>

--- js/scripts.js
+++ js/scripts.js
@@ -5,26 +5,49 @@ $(document).ready(function() {
   let genre = $("input[name='genre']:checked").val();
   console.log(genre);
 
-  if (genre === "celeb1") {
-    $('#scifi').show();
-  } else if (genre === 'celeb2') {
-    $('#action').show();
-  } else if (genre === 'celeb3') {
-    $('#drama').show();
+  // <input class="form-check-input" type="radio" name="genre" id="movieGenre" value="celeb1">
+  // <label class="form-check-label" for="movieGenre">Sci Fi</label>
+
+  // if (genre === "celeb1") {
+  //   $('#scifi').show();
+  // } else if (genre === 'celeb2') {
+  //   $('#action').show();
+  // } else if (genre === 'celeb3') {
+  //   $('#drama').show();
+  // }
+  // $(".genre").text(genre);
+
+  // <div id="scifi">
+  // <p>actor name</p>
+  // </div>
+
+
+  if (genre === "scifi" && scene === "journey") {
+    $('#scifiJourney').show();
+  } else if (genre === "scifi" && scene === "fight") {
+    $('#scifiFight').show();
+  } else if (genre === "scifi" && scene === "romance") {
+    $('#scifiRomance').show();
   }
+
+  if (genre === "action" && scene === "journey") {
+    $('#actionJourney').show();
+  } else if (genre === "action" && scene === "fight") {
+    $('#actionFight').show();
+  } else if (genre === "action" && scene === "romance") {
+    $('#actionRomance').show();
+  }
+
+  if (genre === "drama" && scene === "journey") {
+    $('#dramaJourney').show();
+  } else if (genre === "drama" && scene === "fight") {
+    $('#dramaFight').show();
+  } else if (genre === "drama" && scene === "romance") {
+    $('#dramaRomance').show();
+  }
+
   $(".genre").text(genre);
+  $(".genre").text(scene);
 
-
-  let scene = $("input[name='scene']:checked").val();
-  console.log(scene);
-
-  if (scene === "celeb1") {
-    $('#scifi').show();
-  } else if (scene === 'celeb2') {
-    $('#action').show();
-  } else if (scene === 'celeb3') {
-    $('#drama').show();
-  }
-  $(".scene").text(scene);
   });
 });
