2e3070ae4f8727ce13b194eafaccab83e911547e
Joseph Pearce <joseph.pearce05@gmail.com>
2020-06-04 15:52:54 -0700

    add questions, hide questions, fix logic
    Jerrod Styrk <jstyrk@citadel.edu>

--- js/scripts.js
+++ js/scripts.js
@@ -1,53 +1,49 @@
 $(document).ready(function() {
-  $('.genre-form').submit(function(event) {
+  $('#genre-form').submit(function(event) {
     event.preventDefault();
+    
+    let genre = $("input[name='genre']:checked").val();
+    let scene = $("input[name='scene']:checked").val();
+
+    $('#scifiJourney').hide();
+    $('#scifiFight').hide();
+    $('#scifiRomance').hide();
+    $('#actionJourney').hide();
+    $('#actionFight').hide();
+    $('#actionRomance').hide();
+    $('#dramaJourney').hide();
+    $('#dramaFight').hide();
+    $('#dramaRomance').hide();
+
+    console.log(genre);
+    console.log(scene);
+
   
-  let genre = $("input[name='genre']:checked").val();
-  console.log(genre);
+    if (genre === "scifi" && scene === "journey") {
+      $('#scifiJourney').show();
+    } else if (genre === "scifi" && scene === "fight") {
+      $('#scifiFight').show();
+    } else if (genre === "scifi" && scene === "romance") {
+      $('#scifiRomance').show();
+    }
 
-  // <input class="form-check-input" type="radio" name="genre" id="movieGenre" value="celeb1">
-  // <label class="form-check-label" for="movieGenre">Sci Fi</label>
+    if (genre === "action" && scene === "journey") {
+      $('#actionJourney').show();
+    } else if (genre === "action" && scene === "fight") {
+      $('#actionFight').show();
+    } else if (genre === "action" && scene === "romance") {
+      $('#actionRomance').show();
+    }
 
-  // if (genre === "celeb1") {
-  //   $('#scifi').show();
-  // } else if (genre === 'celeb2') {
-  //   $('#action').show();
-  // } else if (genre === 'celeb3') {
-  //   $('#drama').show();
-  // }
-  // $(".genre").text(genre);
-
-  // <div id="scifi">
-  // <p>actor name</p>
-  // </div>
+    if (genre === "drama" && scene === "journey") {
+      $('#dramaJourney').show();
+    } else if (genre === "drama" && scene === "fight") {
+      $('#dramaFight').show();
+    } else if (genre === "drama" && scene === "romance") {
+      $('#dramaRomance').show();
+    }
 
 
-  if (genre === "scifi" && scene === "journey") {
-    $('#scifiJourney').show();
-  } else if (genre === "scifi" && scene === "fight") {
-    $('#scifiFight').show();
-  } else if (genre === "scifi" && scene === "romance") {
-    $('#scifiRomance').show();
-  }
-
-  if (genre === "action" && scene === "journey") {
-    $('#actionJourney').show();
-  } else if (genre === "action" && scene === "fight") {
-    $('#actionFight').show();
-  } else if (genre === "action" && scene === "romance") {
-    $('#actionRomance').show();
-  }
-
-  if (genre === "drama" && scene === "journey") {
-    $('#dramaJourney').show();
-  } else if (genre === "drama" && scene === "fight") {
-    $('#dramaFight').show();
-  } else if (genre === "drama" && scene === "romance") {
-    $('#dramaRomance').show();
-  }
-
-  $(".genre").text(genre);
-  $(".genre").text(scene);
 
   });
 });
